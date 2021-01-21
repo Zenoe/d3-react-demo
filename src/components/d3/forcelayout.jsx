270f1d60d1f12b56f85a66ebb2b1429bc8cb8c59
zenoe <gauss.cn@gmail.com>
2021-01-21 20:26:52 +0800

    forcelayout

--- src/components/d3/forcelayout.jsx
+++ src/components/d3/forcelayout.jsx
@@ -1,7 +1,8 @@
+// http://bl.ocks.org/sathomas/1ca23ee9588580d768aa
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-
 import {makeStyles } from '@material-ui/core/styles';
+import FlexBox from '@/components/FlexBox';
 
 const d3=require('d3');
 
@@ -20,19 +21,48 @@ const useStyles = makeStyles((theme)=>({
   }
 }));
 
+let simulation
 const render = (containerid, width, height)=>{
   const nodes = [
-    { x:   width/3, y: height/2 },
-    { x: 2*width/3, y: height/2 }
+    { x:   width/3, y: height/4, graph: 0 },
+    { x: 2* width/3,y:height/4, graph: 0 },
+    { x: width/3,   y: height/2, graph: 1 },
+    { x: 2*width/3, y: height/2, graph: 1 },
+
+    { x:   width/3, y: 3*height/4, graph:2 },
+    { x: 2*width/3, y: 3*height/4, graph:2 }
   ];
 
   const links = [
-    {source: 0, target: 1}
+    {source: 0, target: 1},
+    {source: 2, target: 3},
+    {source: 4, target: 5},
   ]
 
   const chart = d3.select(containerid)
-  const simulation = d3.forceSimulation(nodes)
-                  .force("link", d3.forceLink(links).distance(width/2))
+  chart.selectAll('*').remove();
+  simulation = d3.forceSimulation(nodes)
+                       .force("link", d3.forceLink(links)
+                                        .distance((d,i)=>( width* (i+1)/ 3 ))
+                                        // .distance((d,i)=>( 4*width/ 5 ))
+                              // the higher strength, the more rapidly it go apart(or colide when given negative strength)
+                                        .strength((d,i)=> 0.1*(3-i))
+                             )
+
+
+  // simulation.gravity(0)
+  const forceX = d3.forceX().strength(0)
+  const forceY = d3.forceY(height / 2).strength(0)
+  simulation.force('x', forceX)
+            .force('y', forceY)
+
+  simulation.force('charge', d3.forceManyBody().strength(d=>{
+    if(d.graph === 0){
+      return -3
+    }
+    return -300
+  }))
+
 
   const link = chart.selectAll('.link')
                     .data(links)
@@ -44,39 +74,102 @@ const render = (containerid, width, height)=>{
     .enter().append('circle')
     .attr('class', 'node')
     .attr('r', '10')
+    .attr('cx', d=>d.x)
+    .attr('cy', d=>d.y)
 
-  simulation.on('end', ()=>{
-    node.attr('r', width/25)
-        .attr('cx', function(d) { return d.x; })
-        .attr('cy', function(d) { return d.y; });
+  simulation.on('tick', ()=>{
+    if(simulation.fullSpeed){
+      node.attr('cx', (d)=>  d.x )
+          .attr('cy', (d)=>  d.y );
+      link
+        .attr('x1', (d) =>(d.source.x) )
+        .attr('y1', (d) =>(d.source.y) )
+        .attr('x2', (d) =>(d.target.x) )
+        .attr('y2', (d) =>(d.target.y) );
+    }else{
+      node.transition().ease(d3.easeLinear).duration(400)
+          .attr('cx', (d)=>  d.x )
+          .attr('cy', (d)=>  d.y );
+      link.transition().ease(d3.easeLinear).duration(400)
+        .attr('x1', (d) =>(d.source.x) )
+        .attr('y1', (d) =>(d.source.y) )
+        .attr('x2', (d) =>(d.target.x) )
+        .attr('y2', (d) =>(d.target.y) );
+    }
 
-    link.attr('x1', function(d) { return d.source.x; })
-        .attr('y1', function(d) { return d.source.y; })
-        .attr('x2', function(d) { return d.target.x; })
-        .attr('y2', function(d) { return d.target.y; });
+    if(!simulation.fullSpeed){
+      // stop tick triggered by tick1
+      // advance only one step on click
+      simulation.stop()
+    }
+
+    if(simulation.slowMotion){
+      setTimeout(()=>{simulation.restart()}, 400)
+    }
+
   })
+
+  // simulation.on('end', ()=>{
+  //   node.attr('r', width/25)
+  //       // .attr('cx', (d) =>(d.x) )
+  //       // .attr('cy', (d) =>(d.y) );
+
+  //   // link.attr('x1', (d) =>(d.source.x) )
+  //   //     .attr('y1', (d) =>(d.source.y) )
+  //   //     .attr('x2', (d) =>(d.target.x) )
+  //   //     .attr('y2', (d) =>(d.target.y) );
+  // })
 }
 
 const ForceLayout=(props)=>{
   const d3ContainRef = useRef(null)
   const {width, height} = props
 
-  const numNodes = 100;
-  const nodes = d3.range(numNodes).map((d,i)=>({radius: Math.random()*25, category: i%3}))
-
   const cs = useStyles();
+  const tick1 = ()=>{
+    // each click cause tick to execute,then stop
+    simulation.restart();
+  }
+
+  const reset1 = ()=>{
+    if(simulation){
+      simulation.stop()
+    }
+    render(d3ContainRef.current, 4*width/5-40, height)
+  }
+  const play1 = ()=>{
+    simulation.fullSpeed = true;
+    simulation.restart();
+  }
+
+  const slow1 = ()=>{
+    simulation.slowMotion = true;
+    simulation.restart();
+  }
 
   useEffect(()=>{
     if(!d3ContainRef){
       return;
     }
-    render(d3ContainRef.current, width-40, height)
+    render(d3ContainRef.current, 4*width/5-40, height)
   }, [ d3ContainRef.current ])
 
   return(
-    <svg width='100%' height='100%'>
-      <g ref={d3ContainRef} className={cs.root} />
-    </svg>
+    <FlexBox>
+      <svg width='80%' height='100%'>
+        <g ref={d3ContainRef} className={cs.root} />
+      </svg>
+      <FlexBox width='20%' height='100%'>
+        <div width='5%'>
+          <button type='button' id='play' onClick={play1}>play</button>
+          <button type='button' id='slow' onClick={slow1}>slow</button>
+          <button type='button' id='play' onClick={tick1}>tick</button>
+          <button type='button' id='reset' onClick={reset1}>reset</button>
+        </div>
+
+      </FlexBox>
+
+    </FlexBox>
   )
 }
 
